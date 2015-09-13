32702d765f94c24e3ee4b51d18f2b19a64627b7a
James Newton <blackslate@lexogram.com>
2015-09-13 18:54:20 -0400

    Demo of "RangeError: Maximum call stack size exceeded" issue

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 
-console.log ("Script loaded")
-
 if (Meteor.isClient) {
   ;(function (){
-    Meteor.call('getNodes', callback) // or ..., null, callback)
+    Meteor.call('getNodes', callback)
     function callback(error, data) {
-      //console.log(error, data)
       Session.set("nodes", data)
     }
   })()
@@ -13,15 +10,6 @@ if (Meteor.isClient) {
   Template.nodes.helpers({
     nodes: function () {
       return Session.get('nodes')
-    }
-  , selected: function () {
-      return Session.get('selectedNode') || "No nodes selected"
-    }
-  })
-  Template.nodes.events({
-    'click select' : function () {
-      var selectedNode = $("#nodes option:selected").text()
-      Session.set("selectedNode", selectedNode)
     }
   })
 }
@@ -39,49 +27,9 @@ if (Meteor.isServer) {
   )
 
   Meteor.startup(function () {
-
     Meteor.methods({
       getNodes: function () {
-        nodesArray = nodesCursor.fetch()
-  
-        console.log("getNodes called")
-        // [ { hello: 
-        //  { _service: [Object],
-        //    name: 'Hello',
-        //    id: 6,
-        //    labels: [],
-        //    metadata: [Object] },
-        // link: 
-        //  { _service: [Object],
-        //    id: 1,
-        //    type: 'LINK',
-        //    metadata: [Object],
-        //    start: '6',
-        //    end: '42' },
-        // world: 
-        //  { _service: [Object],
-        //    name: 'World',
-        //    id: 42,
-        //    labels: [],
-        //    metadata: [Object] } }
-        // , ... ]
-        
-        var nodes = []
-        var keys
-          , name
-
-        nodesArray.forEach(function (object) { // , index, array) {
-          keys = Object.keys(object)
-          for (var ii=0, key; key=keys[ii]; ii++) {
-            name = object[key].name // may be undefined
-            if (name) {
-              nodes.push({node: name})
-              console.log(ii, key, name)
-            }
-          }
-        })
-
-        console.log(nodes)
+        nodes = nodesCursor.fetch()
         return nodes
       }
     })
