a083aeb5188891d845adeda06edbfae7e188fccc
James Newton <blackslate@lexogram.com>
2015-09-14 11:10:56 -0400

    Add stub for node data
    - {{selectedData}} returns "No data available"
    - Click on node name triggers asynch call to Neo4j for links for node
    - Meteor.Collection( "neo4jData" ) used for dummy non-db message

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 
-console.log ("Script loaded")
+var neo4jData = new Meteor.Collection( "neo4jData" )
+var subscription = "neo4jData"
+
 
 if (Meteor.isClient) {
+   
+  Tracker.autorun( function(){
+    console.log("Auto running")
+    Meteor.subscribe ( subscription )
+    var message = neo4jData.findOne({})
+    if ( message ) {
+      console.log( message.m )
+    }
+  })
+
   ;(function (){
     Meteor.call('getNodes', callback) // or ..., null, callback)
     function callback(error, data) {
-      //console.log(error, data)
-      Session.set("nodes", data)
+      if (!error) {
+        Session.set("nodes", data)
+      }
     }
   })()
 
@@ -17,16 +30,38 @@ if (Meteor.isClient) {
   , selected: function () {
       return Session.get('selectedNode') || "No nodes selected"
     }
+  , selectedData: function () {
+      return Session.get('selectedNodeData') || "No data available"
+    }
   })
   Template.nodes.events({
     'click select' : function () {
-      var selectedNode = $("#nodes option:selected").text()
-      Session.set("selectedNode", selectedNode)
+      var $selector = $("#nodes option:selected")
+      var name = $selector.text()
+      var id = parseInt($selector.val(), 10)
+      Session.set("selectedNode", name)
+      Meteor.call("getLinksForNode", id, callback)
+
+      function callback (error, data) {
+        console.log(error, data)
+        if (!error) {
+          Session.set("selectedNodeData", data)
+        }
+      }
     }
   })
 }
 
 if (Meteor.isServer) {
+  Meteor.publish( subscription, subscribeCallback)
+
+  function subscribeCallback(){
+    console.log("subscribeCallback")
+    var message = "Hello Client"
+    this.added ( subscription, "madeUpId1", { m: message } );
+    this.ready();
+  }
+
   var db = new Neo4jDB(
     'http://localhost:7474'
   , { username: 'neo4j', password: '1234'}
@@ -37,58 +72,109 @@ if (Meteor.isServer) {
     '(hello {name:"Hello"})-[link:LINK]->(world {name: "World"}) ' +
     'RETURN hello, link, world'
   )
+  // console.log(nodesCursor)
+  // { _cursor: [ { hello: [Object], link: [Object], world: [Object] } ],
+  // length: 1,
+  // _current: 0,
+  // hasNext: false,
+  // hasPrevious: false }
+
+  var linksForNodeQuery = 
+    'MATCH (node)-[link]->(endpoint) ' +
+    'WHERE id(node) = {id} ' +
+    'RETURN node, link, endpoint'
 
   Meteor.startup(function () {
     Meteor.methods({
       getNodes: function () {
-        nodesArray = fetchNodes(nodesCursor)
+        nodesArray = fetchResult(nodesCursor).nodes
         return nodesArray
       }
-    })
-  })
+    , getLinksForNode: function (nodeId) {
+        var options = { id: nodeId }
+        console.log(linksForNodeQuery, options)
 
-  function fetchNodes(cursor) {
-    var nodesArray = cursor.fetch()
-    var nodes = []
-    var ids = []
-    var keys
-      , node
-
-    nodesArray.forEach(function (object) {
-      keys = Object.keys(object)
-      for (var ii=0, key; key=keys[ii]; ii++) {
-       node = object[key]
-        if (node.labels instanceof Array) {
-          if (ids.indexOf(node.id) < 0) {
-            node = clone(node)
-            nodes.push(node)
-            ids.push(node.id)
+        db.query(linksForNodeQuery, options, callback)
+        function callback(error, cursor) {
+          console.log(error, cursor)
+          // cursor =
+          // { _cursor: [ 
+          //     { node: [Object]
+          //     , link: [Object]
+          //     , endpoint: [Object]
+          //     }
+          //   ]
+          // , length: 1
+          // , _current: 0
+          // , hasNext: false
+          // , hasPrevious: false
+          // }
+          
+          if (!error) {
+            result = fetchResult(cursor)
+// console.log(result)
+// { nodes: 
+//   [ { name: 'Hello', id: 36, labels: [], metadata: [Object] },
+//     { name: 'World', id: 37, labels: [], metadata: [Object] } ],
+//   links: 
+//   [ { id: 13,
+//       type: 'LINK',
+//       metadata: [Object],
+//       start: '36',
+//       end: '37' } ] }
           }
         }
       }
     })
-    return nodes
-  }
+  })
 
-  function clone(node) {
-    var copy = {}
-    var keys = Object.keys(node)
-    var value
-    
-    keys.forEach(function (key, index, array){
-      // The "_db" object contains circular references. Drop it.
-      if (key !== "_db") {
-        value = node[key]
+  function fetchResult(cursor) {
+    var result = cursor.fetch()
+    var nodes = []
+    var links = []
+    var nodeIds = [] // relationships 
+    var keys
+      , value
+      , array
 
-        if (typeof value === "object") {
-          // Ensure that the object contains no _db property
-          copy[key] = clone(value)
+    result.forEach(function (object) {
+      keys = Object.keys(object)
+      for (var ii=0, key; key=keys[ii]; ii++) {
+       value = object[key]
+        if (value.labels instanceof Array) {
+          if (nodeIds.indexOf(value.id) < 0) {           
+            nodeIds.push(value.id)
+          } else {
+            return
+          }
+
+          array = nodes
         } else {
-          copy[key] = value
+          array = links
         }
+        
+        value = cloneWithout_service(value)
+        array.push(value)
       }
     })
 
-    return copy
+    return { nodes: nodes, links: links }
+ 
+    function cloneWithout_service(object) {
+      var clone = {}
+      var keys = Object.keys(object)
+      var value
+      
+      keys.forEach(function (key) { //, index, array){
+        // The "_service" object contains REST API URLs and pointers
+        // to the server-side db object. It is not needed in the
+        // client. Drop it.
+        if (key !== "_service") {
+          clone[key] = object[key]
+        }
+      })
+
+      return clone
+    }
   }
 }
